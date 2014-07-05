f1a04433854ad18f35af1f1d8f53f5ca9e307075
Guenter Roeck <linux@roeck-us.net>
2014-07-05 09:56:22 -0700

    g760a.sh: Use common test framework
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- scripts/g760a.sh
+++ scripts/g760a.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+i2c_addr=3f
+
+dir=$(dirname $0)
+. ${dir}/common.sh
+
 modprobe -r i2c-stub 2>/dev/null
 modprobe i2c-stub chip_addr=0x3f
 if [ $? -ne 0 ]
@@ -10,47 +15,27 @@ fi
 
 adapter=$(grep "SMBus stub driver" /sys/class/i2c-adapter/*/name | cut -f1 -d: | cut -f5 -d/ | cut -f2 -d-)
 
-i2cset -f -y ${adapter} 0x3f 0x0 0x7f b
-i2cset -f -y ${adapter} 0x3f 0x1 0x80 b
-i2cset -f -y ${adapter} 0x3f 0x2 0x00 b
+i2cset -f -y ${adapter} 0x${i2c_addr} 0x0 0x7f b
+i2cset -f -y ${adapter} 0x${i2c_addr} 0x1 0x80 b
+i2cset -f -y ${adapter} 0x${i2c_addr} 0x2 0x00 b
 
-echo g760a 0x3f > /sys/class/i2c-adapter/i2c-${adapter}/new_device
+echo g760a 0x${i2c_addr} > /sys/class/i2c-adapter/i2c-${adapter}/new_device
 
-hwmon_dev=$(ls /sys/class/i2c-adapter/i2c-${adapter}/${adapter}-003f/hwmon 2>/dev/null)
-if [ "${hwmon_dev}" = "" ]
+base=$(getbase ${adapter} 00${i2c_addr})
+if [ "${base}" = "" -o ! -d "${base}" ]
 then
 	echo fail: No hwmon device
 	exit 1
 fi
 
-base=/sys/class/i2c-adapter/i2c-${adapter}/${adapter}-003f
-if [ ! -e ${base}/fan1_input ]
-then
-	base=${hwmon_dev}
-fi
+cd ${base}
 
-if [ ! -e ${base}/fan1_alarm -o ! -e ${base}/fan1_input -o ! -e ${base}/pwm1 ]
-then
-	echo fail: Missing attribute
-	exit 1
-fi
+attrs=(fan1_alarm fan1_input pwm1)
+vals=(0 3840 128)
 
-if [ $(cat ${base}/fan1_alarm) -ne 0 ]
-then
-	echo fail: fan1_alarm
-	exit 1
-fi
-
-if [ $(cat ${base}/fan1_input) -ne 3840 ]
-then
-	echo fail: fan1_input
-	exit 1
-fi
-
-if [ $(cat ${base}/pwm1) -ne 128 ]
-then
-	echo fail: pwm1
-	exit 1
-fi
+dotest attrs[@] vals[@]
+rv=$?
 
 modprobe -r i2c-stub 2>/dev/null
+
+exit ${rv}
