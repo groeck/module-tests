d8dad08d56afc47fb774c112d5f4d370cc86cc30
Guenter Roeck <linux@roeck-us.net>
2014-12-05 09:42:39 -0800

    Add test script for tmp401 variants
    tmp401, tmp411 missing
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

---
+++ scripts/tmp401.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+i2c_addr=0x4c
+
+dir=$(dirname $0)
+. ${dir}/common.sh
+
+load_i2c_stub ${i2c_addr}
+
+regs=(
+	1a 1b 00 00 05 55 00 55 00 00 05 55 00 55 00 0d
+	70 00 00 00 00 90 00 00 00 55 1c ff ff ff ff 00
+	55 0a 70 ff ff 0f ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
+	ff ff ff ff ff ff ff ff ff ff ff ff 00 35 55 35
+)
+
+i=0
+while [ $i -lt ${#regs[*]} ]
+do
+	i2cset -f -y ${i2c_adapter} ${i2c_addr} $i 0x${regs[$i]} b
+	i=$(($i + 1))
+done
+
+do_instantiate tmp435 ${i2c_addr}
+
+getbasedir ${i2c_addr}
+
+cd ${basedir}
+
+attrs=(temp1_crit temp1_crit_alarm temp1_crit_hyst temp1_input temp1_max
+	temp1_max_alarm temp1_min temp1_min_alarm
+	temp2_crit temp2_crit_alarm temp2_crit_hyst temp2_fault temp2_input
+	temp2_max temp2_max_alarm temp2_min temp2_min_alarm
+	update_interval
+)
+vals=(85000 0 75000 26563 85000 0 0 0 85000 0 75000 0 27438 85000 0 0 0 500)
+
+dotest attrs[@] vals[@]
+rv=$?
+
+for t in $(seq 1 2)
+do
+	# We can not test ranges on min/max attributes since the
+	# read and write registers are different.
+	check_range -b ${basedir} -d 500 -r -q -w 2 temp${t}_crit
+	rv=$(($? + ${rv}))
+	if [ $t -eq 1 ]
+	then
+	    check_range -b ${basedir} -d 500 -r -q -w 2 temp${t}_crit_hyst
+	    rv=$(($? + ${rv}))
+	fi
+done
+
+check_range -b ${basedir} -d 4000 -r -q update_interval
+rv=$(($? + ${rv}))
+
+do_remove ${i2c_addr}
+
+i2cset -y -f ${i2c_adapter} ${i2c_addr} 0xff 0x31 b	# tmp431
+do_instantiate tmp431 ${i2c_addr}
+
+cd ${basedir}
+
+dotest attrs[@] vals[@]
+rv=$(($? + ${rv}))
+
+do_remove ${i2c_addr}
+
+i2cset -y -f ${i2c_adapter} ${i2c_addr} 0xff 0x32 b	# tmp432
+do_instantiate tmp432 ${i2c_addr}
+
+cd ${basedir}
+
+attrs=(temp1_crit temp1_crit_alarm temp1_crit_hyst temp1_input
+	temp1_max temp1_max_alarm temp1_min temp1_min_alarm
+	temp2_crit temp2_crit_alarm temp2_crit_hyst temp2_fault
+	temp2_input temp2_max temp2_max_alarm temp2_min temp2_min_alarm
+	temp3_crit temp3_crit_alarm temp3_crit_hyst temp3_fault
+	temp3_input temp3_max temp3_max_alarm temp3_min temp3_min_alarm
+	update_interval
+)
+vals=(85000 1 75000 26996 85996 1 996 1 85000 1 75000 1 27438 85000 1 0
+	1 28000 1 18000 1 255996 144000 1 0 1 500)
+
+dotest attrs[@] vals[@]
+rv=$(($? + ${rv}))
+
+for t in $(seq 1 3)
+do
+	# We can not test ranges on min/max attributes since the
+	# read and write registers are different.
+	check_range -b ${basedir} -d 500 -r -q -w 2 temp${t}_crit
+	rv=$(($? + ${rv}))
+	if [ $t -eq 1 ]
+	then
+	    check_range -b ${basedir} -d 500 -r -q -w 2 temp${t}_crit_hyst
+	    rv=$(($? + ${rv}))
+	fi
+done
+
+check_range -b ${basedir} -d 4000 -r -q update_interval
+rv=$(($? + ${rv}))
+
+modprobe -r i2c-stub 2>/dev/null
+modprobe -r tmp401 2>/dev/null
+
+exit ${rv}
