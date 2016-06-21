9be33f1a72a86bce06ce199dc6a948489b37e6d5
Guenter Roeck <linux@roeck-us.net>
2016-06-20 20:48:19 -0700

    scripts: lm75: Add test for ds75
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- scripts/lm75.sh
+++ scripts/lm75.sh
@@ -5,9 +5,7 @@ i2c_addr=0x4c
 dir=$(dirname $0)
 . ${dir}/common.sh
 
-load_i2c_stub ${i2c_addr}
-
-regs=(
+regs_lm75=(
 	7f1c ff00 7f4b 7f50 7f50 7f50 7f50 7f50
 	7f1c ff00 7f4b 7f50 7f50 7f50 7f50 7f50
 	7f1c ff00 7f4b 7f50 7f50 7f50 7f50 7f50
@@ -41,32 +39,59 @@ regs=(
 	7f1c ff00 7f4b 7f50 7f50 7f50 7f50 7f50
 	7f1c ff00 7f4b 7f50 7f50 7f50 7f50 7f50
 )
-i=0
-while [ $i -lt ${#regs[*]} ]
-do
+
+attrs_lm75=(temp1_input temp1_max temp1_max_hyst)
+vals_lm75=(28000 80000 75000)
+
+regs_ds75=(
+	7f1c 0030 004b 0050 0050 0050 0050 0050
+	0050 0050 0050 0050 0050 0050 0050 0050
+)
+
+attrs_ds75=(temp1_input temp1_max temp1_max_hyst)
+vals_ds75=(28375 80000 75000)
+
+runtest()
+{
+    local chip=$1
+    local regs=("${!2}")
+    local attrs=("${!3}")
+    local vals=("${!4}")
+    local rv
+    local i
+
+    load_i2c_stub ${i2c_addr}
+
+    i=0
+    while [ $i -lt ${#regs[*]} ]
+    do
 	i2cset -f -y ${i2c_adapter} ${i2c_addr} $i 0x${regs[$i]} w
 	i=$(($i + 1))
-done
+    done
 
-# may be auto-detected
-do_instantiate lm75 ${i2c_addr} 2>/dev/null
+    # may be auto-detected
+    do_instantiate ${chip} ${i2c_addr} 2>/dev/null
 
-getbasedir ${i2c_addr}
+    getbasedir ${i2c_addr}
 
-cd ${basedir}
+    cd ${basedir}
 
-attrs=(temp1_input temp1_max temp1_max_hyst)
+    dotest attrs[@] vals[@]
+    rv=$?
 
-vals=(28000 80000 75000)
+    check_range -b ${basedir} -s 100 -d 200 -r -q temp1_max
+    rv=$(($? + ${rv}))
+    check_range -b ${basedir} -s 100 -d 200 -r -q temp1_max_hyst
+    rv=$(($? + ${rv}))
 
-dotest attrs[@] vals[@]
+    modprobe -r i2c-stub 2>/dev/null
+
+    return ${rv}
+}
+
+runtest lm75 regs_lm75[@] attrs_lm75[@] vals_lm75[@]
 rv=$?
-
-check_range -b ${basedir} -s 100 -d 200 -r -q temp1_max
+runtest ds75 regs_ds75[@] attrs_ds75[@] vals_ds75[@]
 rv=$(($? + ${rv}))
-check_range -b ${basedir} -s 100 -d 200 -r -q temp1_max_hyst
-rv=$(($? + ${rv}))
-
-modprobe -r i2c-stub 2>/dev/null
 
 exit ${rv}
