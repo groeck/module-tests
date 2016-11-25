b49fe339f138dceb1c882a86f39378c5d983d1ac
Guenter Roeck <linux@roeck-us.net>
2016-11-25 15:52:54 -0800

    scripts: Add runall.sh script to run all tests
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

---
+++ scripts/runall.sh
@@ -0,0 +1,50 @@
+set -v
+
+./ad7414.sh
+./ad7418.sh
+./adm1021.sh
+./adm1025.sh
+./adm1026.sh
+./adm1029.sh
+./adm1031.sh
+./adm9240.sh
+./ads7828.sh
+# ./adt7411-new.sh
+./adt7411.sh
+./adt7462.sh
+./adt7470.sh
+./amc6821.sh
+./dme1737.sh
+./ds620.sh
+./emc2103.sh
+./emc6w201.sh
+./g760a.sh
+./g762.sh
+./gl518sm.sh
+./gl520sm.sh
+# not built by default
+# ./htu21.sh
+./ina3221.sh
+./jc42.sh
+./lineage-pem.sh
+./lm75.sh
+./lm77.sh
+./lm78.sh
+./lm85.sh
+./lm87.sh
+./lm90.sh
+./lm92.sh
+./lm93.sh
+./lm95241.sh
+./lm95245.sh
+./ltc4245.sh
+# Incomplete / testing
+# ./max31790.sh
+./nct7802.sh
+./nct7904.sh
+./sht21.sh
+./smsc47m192.sh
+./thmc50.sh
+./tmp102.sh
+./tmp401.sh
+./tmp421.sh
