a354c68d985185d7d16b236dc22447c3ce00f478
Guenter Roeck <linux@roeck-us.net>
2021-03-10 16:38:46 -0800

    adm9240: Modernize, add lm81 test
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- scripts/adm9240.sh
+++ scripts/adm9240.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-i2c_addr=2c
+i2c_addr=0x2c
 
 dir=$(dirname $0)
 . ${dir}/common.sh
 
-modprobe -r i2c-stub 2>/dev/null
-modprobe i2c-stub chip_addr=0x${i2c_addr}
-if [ $? -ne 0 ]
-then
-	echo must be root
-	exit 1
-fi
-
-adapter=$(grep "SMBus stub driver" /sys/class/i2c-adapter/*/name | cut -f1 -d: | cut -f5 -d/ | cut -f2 -d-)
-
-regs=(b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1
+adm9240_regs=(
+	b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1
 	b1 b1 b1 b1 b1 00 00 00 00 ff ff ff ff ff ff ff
 	74 8f c2 c7 c5 b7 b6 21 60 ff ff 7e 69 97 82 ca
 	b6 ca b6 d3 ad ba a0 a0 a0 32 2d 8c ff ff 23 02
@@ -33,25 +24,7 @@ regs=(b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1 b1
 	01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
 )
 
-i=0
-while [ $i -lt ${#regs[*]} ]
-do
-	i2cset -f -y ${adapter} 0x${i2c_addr} $i 0x${regs[$i]} b
-	i=$(($i + 1))
-done
-
-echo adm9240 0x${i2c_addr} > /sys/class/i2c-adapter/i2c-${adapter}/new_device
-
-base=$(getbase ${adapter} 00${i2c_addr})
-if [ "${base}" = "" -o ! -d "${base}" ]
-then
-	echo fail: No hwmon device
-	exit 1
-fi
-
-cd ${base}
-
-attrs=(name alarms aout_output cpu0_vid
+adm9240_attrs=(name alarms aout_output cpu0_vid
 	fan1_alarm fan1_div fan1_input fan1_min
 	fan2_alarm fan2_div fan2_input fan2_min
 	in0_alarm in0_input in0_max in0_min
@@ -64,44 +37,97 @@ attrs=(name alarms aout_output cpu0_vid
 	temp1_alarm temp1_input temp1_max temp1_max_hyst
 	)
 
-vals=(adm9240 0 1250 0 0 8 1758 1205 0 4 0 0 0 1510 1641 1367 0
+adm9240_vals=(adm9240 0 1250 0 0 8 1758 1205 0 4 0 0 0 1510 1641 1367 0
 	2011 2123 1828 0 3334 3472 3128 0 5182 5260 4740 0 12313 13188 10813 0
 	2573 2616 2250 0 0 33000 50000 45000
 	)
 
-rv=0
-dotest attrs[@] vals[@]
-rv=$(($? + ${rv}))
+lm81_regs=(
+	47 47 47 47 47 47 47 47 47 47 47 47 47 47 47 47
+	47 81 24 03 94 00 00 00 00 ff ff ff ff ff ff ff
+	bf cb c1 00 c0 47 ec 24 ff ff 65 ff 00 ff 00 ff
+	00 ff 00 ff 00 ff 00 71 a9 7f 7f ff ff 58 01 04
+	01 08 00 00 00 00 00 50 2f 80 80 01 44 00 00 00
+	00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	00 81 24 03 94 00 00 00 00 ff ff ff ff ff ff ff
+	bf cb c1 00 c0 47 ec 24 ff ff 65 ff 00 ff 00 ff
+	00 ff 00 ff 00 ff 00 71 a9 7f 7f ff ff 58 01 04
+	01 00 00 00 00 00 00 50 2f 80 80 01 44 00 00 00
+	00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+	00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
+)
 
-for i in $(seq 0 5)
-do
-	check_range -b ${base} -d 31 -r -q "in${i}_min"
-	rv=$(($? + ${rv}))
-	check_range -b ${base} -d 31 -r -q "in${i}_max"
-	rv=$(($? + ${rv}))
-done
+lm81_vals=(lm81 8 1250 0 0 2 0 0 0 2 0 0 0 2487 3320 0
+	0 2855 3586 0 0 3317 4383 0 1 0 6641 0 0 12000 15938 0
+	0 998 3586 0 0 0 36000 127000 127000
+)
 
-for i in $(seq 1 1)
-do
-	check_range -b ${base} -d 500 -r -q temp${i}_max
-	rv=$(($? + ${rv}))
-	check_range -b ${base} -d 500 -r -q temp${i}_max_hyst
-	rv=$(($? + ${rv}))
-done
+runtest()
+{
+    local chip=$1
+    local regs=("${!2}")
+    local attrs=("${!3}")
+    local vals=("${!4}")
+    local rv
 
-for i in $(seq 1 2)
-do
-	check_range -b ${base} -l 0 -u 20000 -d 500 -r -q fan${i}_min
-	rv=$(($? + ${rv}))
-done
+    echo Testing ${chip} ...
 
-check_range -b ${base} -r -q -d 2 aout_output
-rv=$(($? + ${rv}))
+    load_i2c_stub ${i2c_addr}
 
-check_range -b ${base} -l 0 -u 0 -d 0 -r -q intrusion0_alarm
-rv=$(($? + ${rv}))
+    install_regs ${i2c_adapter} ${i2c_addr} regs[@] b
 
-modprobe -r i2c-stub 2>/dev/null
+    do_instantiate ${chip} ${i2c_addr} 2>/dev/null
+
+    getbasedir ${i2c_addr}
+
+    cd ${basedir}
+
+    dotest attrs[@] vals[@]
+    rv=${rv}
+
+    for i in $(seq 0 5)
+    do
+	check_range -b ${basedir} -d 31 -r -q "in${i}_min"
+	rv=$(($? + rv))
+	check_range -b ${basedir} -d 31 -r -q "in${i}_max"
+	rv=$(($? + rv))
+    done
+
+    for i in $(seq 1 1)
+    do
+	check_range -b ${basedir} -d 500 -r -q temp${i}_max
+	rv=$(($? + rv))
+	check_range -b ${basedir} -d 500 -r -q temp${i}_max_hyst
+	rv=$(($? + rv))
+    done
+
+    for i in $(seq 1 2)
+    do
+	check_range -b ${basedir} -l 0 -u 20000 -d 500 -r -q fan${i}_min
+	rv=$(($? + rv))
+    done
+
+    check_range -b ${basedir} -r -q -d 2 aout_output
+    rv=$(($? + rv))
+
+    check_range -b ${basedir} -l 0 -u 0 -d 0 -r -q intrusion0_alarm
+    rv=$(($? + rv))
+
+    modprobe -r i2c-stub 2>/dev/null
+
+    return ${rv}
+}
+
+runtest adm9240 adm9240_regs[@] adm9240_attrs[@] adm9240_vals[@]
+rv=$?
+
+runtest lm81 lm81_regs[@] adm9240_attrs[@] lm81_vals[@]
+rv=$((rv + $?))
+
 modprobe -r adm9240
 
 exit ${rv}
