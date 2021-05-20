75865dfc498e0a2a779bed37d8a512092f28aa21
Guenter Roeck <linux@roeck-us.net>
2021-05-20 03:46:44 -0700

    max31790: Various improvements
    Add volatile register checks, with fixups
    Registers are 8 bit wide, not 16 bit
    Don't write non-existing registers since it is pointless
    Support fixed vesion of driver (pxmX_enable fixes)
    Add check with chip configuration for 12 fans and no pwm controls
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- scripts/max31790.sh
+++ scripts/max31790.sh
@@ -5,6 +5,26 @@ i2c_addr=20
 dir=$(dirname $0)
 . ${dir}/common.sh
 
+fixup_writeattr()
+{
+    local attr="${1##*/}"
+    local value="$2"
+    local index
+    local reg
+
+    # Writes into pwm attributes are written into registers 0x40..0x4a
+    # but read back from 0x30..0x3a.
+    case "${attr}" in
+    "pwm1"|"pwm2"|"pwm3"|"pwm4"|"pwm5"|"pwm6")
+	index="$((${attr//pwm/} - 1))"
+	reg="$((index * 2 + 0x30))"
+	i2cset -f -y ${adapter} 0x${i2c_addr} ${reg} ${value}
+	;;
+    *)
+	;;
+    esac
+}
+
 modprobe -r i2c-stub 2>/dev/null
 modprobe i2c-stub chip_addr=0x${i2c_addr}
 if [ $? -ne 0 ]
@@ -15,42 +35,24 @@ fi
 
 adapter=$(grep "SMBus stub driver" /sys/class/i2c-adapter/*/name | cut -f1 -d: | cut -f5 -d/ | cut -f2 -d-)
 
-regs=(
-	bb27 40bb 4040 4040 4040 4040 4040 4c40
-	4c4c 4c4c 4c4c 4c4c 4c4c 004c 0000 0000
-	0000 3f00 3f3f 453f 0045 0000 0000 ff00
-	e0ff ffe0 e0ff ffe0 e0ff ffe0 e0ff ffe0
-	e0ff ffe0 e0ff ffe0 e0ff ffe0 e0ff ffe0
-	e0ff ffe0 e0ff ffe0 e0ff ffe0 e0ff 80e0
-	0080 8000 0080 8000 0080 8000 0080 8000
-	0080 8000 0080 0000 0000 0000 0000 8000
-	0080 8000 0080 8000 0080 8000 0080 8000
-	0080 8000 0080 0000 0000 0000 0000 3c00
-	003c 3c00 003c 3c00 003c 3c00 003c 3c00
-	003c 3c00 003c 0000 0000 0000 0000 0000
-	0000 0000 0000 0000 0000 0000 0000 ff00
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff 00ff
-	0000 0000 0000 0000 0000 0000 0000 ff00
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff ffff
-	ffff ffff ffff ffff ffff ffff ffff 27ff
+regs=(	27 bb 48 48 48 48 48 48
+	4c 4c 4c 4c 4c 4c 00 00
+	00 00 3f 3f 45 00 00 00
+	ff e0 ff e0 ff e0 ff e0
+	ff e0 ff e0 ff e0 ff e0
+	ff e0 ff e0 ff e0 ff e0
+	80 00 80 00 80 00 80 00
+	80 00 80 00 00 00 00 00
+	80 00 80 00 80 00 80 00
+	80 00 80 00 00 00 00 00
+	3c 00 3c 00 3c 00 3c 00
+	3c 00 3c 00 00 00 00 00
+	00 00 00 00 00 00 00 00
 )
 
-install_regs ${adapter} ${i2c_addr} regs[@] w
+install_regs ${adapter} ${i2c_addr} regs[@] b
+
+# i2cdump -y -f ${adapter} 0x${i2c_addr} b
 
 echo max31790 0x${i2c_addr} > /sys/class/i2c-adapter/i2c-${adapter}/new_device 2>/dev/null
 
@@ -61,7 +63,7 @@ then
 	exit 1
 fi
 
-pushd ${base}
+pushd ${base} >/dev/null
 
 attrs=(fan1_fault fan1_input fan1_target
 	fan2_fault fan2_input fan2_target
@@ -81,9 +83,9 @@ vals=(0 480 2048
 	0 480 2048
 	0 480 2048
 	max31790
-	128 0 128 0
-	128 0 128 0
-	128 0 128 0)
+	128 1 128 1
+	128 1 128 1
+	128 1 128 1)
 
 dotest attrs[@] vals[@]
 rv=$?
@@ -102,18 +104,74 @@ do
 	rv=$(($? + ${rv}))
 done
 
-popd
+# Registers 0x18 .. 0x2e are volatile, but nonlinear. Check value change only.
+# There is a total of 12 fanX_input attributes, but only 6 are active for
+# the given configuration.
+
+for index in $(seq 1 6); do
+    reg=$((0x16 + (index * 2)))
+    check_volatile -c "${adapter}" "${i2c_addr}" "${reg}" b 0x10 0xff 0x10 "fan${index}_input"
+    rv=$(($? + ${rv}))
+done
+# Registers 0x30 .. 0x3a are 16-bit pwm duty cycle registers.
+for index in $(seq 1 6); do
+    reg=$((0x2e + (index * 2)))
+    check_volatile "${adapter}" "${i2c_addr}" ${reg} b 0x10 0xff 0x10 "pwm${index}" 16 16
+    rv=$(($? + ${rv}))
+done
+
+popd >/dev/null
 
 modprobe -r max31790
 
-i2cset -f -y ${adapter} 0x${i2c_addr} 0x02 0x41 b
+# Reconfigure the chip to monitor 12 fan speeds instead of 6 fan speeds
+# and 6 pwm controls
+regs=(	27 bb 49 49 49 49 49 49
+	4c 4c 4c 4c 4c 4c 00 00
+	00 00 3f 3f 45 00 00 00
+	ff e0 ff e0 ff e0 ff e0
+	ff e0 ff e0 ff e0 ff e0
+	ff e0 ff e0 ff e0 ff e0
+	80 00 80 00 80 00 80 00
+	80 00 80 00 00 00 00 00
+	80 00 80 00 80 00 80 00
+	80 00 80 00 00 00 00 00
+	3c 00 3c 00 3c 00 3c 00
+	3c 00 3c 00 00 00 00 00
+	00 00 00 00 00 00 00 00
+)
+
+install_regs ${adapter} ${i2c_addr} regs[@] b
 
 modprobe max31790
 
 base=$(getbase ${adapter} 00${i2c_addr})
+pushd ${base} >/dev/null
 
-cd ${base}
-ls -l
+attrs=(fan1_fault fan1_input fan2_fault fan2_input
+	fan3_fault fan3_input fan4_fault fan4_input
+	fan5_fault fan5_input fan6_fault fan6_input
+	fan7_fault fan7_input fan8_fault fan8_input
+	fan9_fault fan9_input fan10_fault fan10_input
+	fan11_fault fan11_input fan12_fault fan12_input
+	name)
+
+vals=(0 480 0 480 0 480 0 480
+	0 480 0 480 0 480 0 480
+	0 480 0 480 0 480 0 480
+	max31790)
+
+dotest attrs[@] vals[@]
+rv=$(($? + ${rv}))
+
+# check all 12 fan input attributes
+for index in $(seq 1 12); do
+    reg=$((0x16 + (index * 2)))
+    check_volatile -c "${adapter}" "${i2c_addr}" "${reg}" b 0x10 0xff 0x10 "fan${index}_input"
+    rv=$(($? + ${rv}))
+done
+
+popd >/dev/null
 
 modprobe -r i2c-stub 2>/dev/null
 modprobe -r max31790
