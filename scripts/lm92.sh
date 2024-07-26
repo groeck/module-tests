2f573c8899010528a655d0f18ce81954d87b3837
Guenter Roeck <linux@roeck-us.net>
2024-07-25 18:45:35 -0700

    lm92: Modernize, add tests for max6635
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- scripts/lm92.sh
+++ scripts/lm92.sh
@@ -1,89 +1,89 @@
 #!/bin/bash
 
-i2c_addr=48
+i2c_addr=0x48
+driver=lm92
 
 dir=$(dirname $0)
 . ${dir}/common.sh
 
-modprobe -r i2c-stub 2>/dev/null
-modprobe i2c-stub chip_addr=0x${i2c_addr}
-if [ $? -ne 0 ]
-then
-	echo must be root
-	exit 1
-fi
-
-adapter=$(grep "SMBus stub driver" /sys/class/i2c-adapter/*/name | cut -f1 -d: | cut -f5 -d/ | cut -f2 -d-)
-
-regs=(
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
-	380c b800 0001 0028 0005 0020 0020 0180
+regs_lm92=(
+	3a0c b800 0001 0028 0005 0020 0020 0180
+	3a0c b800 0001 0028 0005 0020 0020 0180
 )
 
-install_regs ${adapter} ${i2c_addr} regs[@] w
+regs_max6635=(
+	390c b800 0001 0028 0005 0020 0020 0020
+	390c b800 0001 0028 0005 0020 0020 0020
+)
 
-echo lm92 0x${i2c_addr} > /sys/class/i2c-adapter/i2c-${adapter}/new_device 2>/dev/null
-
-base=$(getbase ${adapter} 00${i2c_addr})
-if [ "${base}" = "" -o ! -d "${base}" ]
-then
-	echo fail: No hwmon device
-	exit 1
-fi
-
-cd ${base}
-
-attrs=(name alarms temp1_crit temp1_crit_alarm temp1_crit_hyst temp1_input temp1_max
+attrs_lm92=(name alarms temp1_crit temp1_crit_alarm temp1_crit_hyst temp1_input temp1_max
 	temp1_max_alarm temp1_max_hyst temp1_min temp1_min_alarm temp1_min_hyst)
 
-vals=(lm92 0 80000 0 78000 24437 64000 0 62000 10000 0 12000)
+vals_lm92=(lm92 2 80000 0 78000 24437 64000 1 62000 10000 0 12000)
 
-dotest attrs[@] vals[@]
+vals_max6635=(max6635 1 80000 0 78000 24437 64000 0 62000 10000 1 12000)
+
+permissions_lm92=(
+	"-r--r--r--"
+	"-r--r--r--"
+	"-rw-r--r--"
+	"-r--r--r--"
+	"-rw-r--r--"
+	"-r--r--r--"
+	"-rw-r--r--"
+	"-r--r--r--"
+	"-r--r--r--"
+	"-rw-r--r--"
+	"-r--r--r--"
+	"-r--r--r--"
+)
+
+runtest()
+{
+    local chip=$1
+    local regs=("${!2}")
+    local attrs=("${!3}")
+    local vals=("${!4}")
+    local permissions=("${!5}")
+    local rv
+
+    echo Testing ${chip} ...
+
+    modprobe -r ${driver}
+
+    load_i2c_stub ${i2c_addr} width=16
+
+    install_regs ${i2c_adapter} ${i2c_addr} regs[@] w
+
+    # may be auto-detected
+    do_instantiate ${chip} ${i2c_addr} 2>/dev/null
+
+    getbasedir ${i2c_addr}
+
+    cd ${basedir}
+
+    # i2cdump -y -f ${i2c_adapter} ${i2c_addr} w
+    # grep . *
+
+    dotest attrs[@] vals[@]
+    rv=$?
+
+    check_range -d 800 -r -q temp1_min
+    rv=$(($? + rv))
+    check_range -d 800 -r -q temp1_max
+    rv=$(($? + rv))
+    check_range -d 800 -r -q temp1_crit
+    rv=$(($? + rv))
+    check_range -d 800 -r -q temp1_crit_hyst
+    rv=$(($? + rv))
+
+    modprobe -r i2c-stub 2>/dev/null
+    return ${rv}
+}
+
+runtest lm92 regs_lm92[@] attrs_lm92[@] vals_lm92[@] permissions_lm92[@]
 rv=$?
-
-for t in $(seq 1 1)
-do
-	check_range -b ${base} -d 50 -r -q -w 2 temp${t}_min
-	rv=$(($? + ${rv}))
-	check_range -b ${base} -d 50 -r -q -w 2 temp${t}_max
-	rv=$(($? + ${rv}))
-	check_range -b ${base} -d 50 -r -q -w 2 temp${t}_crit
-	rv=$(($? + ${rv}))
-	check_range -b ${base} -d 50 -r -q -w 2 temp${t}_crit_hyst
-	rv=$(($? + ${rv}))
-done
-
-modprobe -r i2c-stub 2>/dev/null
-modprobe -r lm92
+runtest max6635 regs_max6635[@] attrs_lm92[@] vals_max6635[@] permissions_lm92[@]
+rv=$((rv + $?))
 
 exit ${rv}
